664d085623f04408c6ec5ad7d1e16be0933d4a7a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-12 07:49:32 +0500

    create new django project with site files

---
+++ staticfiles/static/js/master.js
@@ -0,0 +1,22 @@
+$(".login").click(()=>{
+    $("#account_modal").show()
+})
+
+
+
+// function siteHost()
+// {
+//     var proto = window.location.protocol;
+//     var host = window.location.hostname;
+//     var port = window.location.port;
+
+//     var site = proto +"//"+ host;
+//     if(port != 80) {
+//         site+=":"+port;
+//     }
+//     return site;
+// }
+
+
+
+
