58506c6cbac233477995c992cd8a59e76af68989
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-10-18 10:35:30 +0500

    update

--- kinobase/settings.py
+++ kinobase/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from .config import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
